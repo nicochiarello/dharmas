55a95e0e365379ebd8e7b4ec75ac0c93144f26b5
Nicolas Chiarello <nicolasch.fcm@gmail.com>
2022-03-28 11:49:25 -0300

    structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Calendario from './Pages/Calendario';
+import Contacto from './Pages/Contacto';
+import Cursos from './Pages/Cursos';
+import Index from './Pages/Index';
+import Masajes from './Pages/Masajes';
+import SobreMi from './Pages/SobreMi';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <Routes>
+      <Route exact path='/' element={<Index/>}/>
+      <Route exact path='/about' element={<SobreMi/>}/>
+      <Route exact path='/cursos' element={<Cursos/>}/>
+      <Route exact path='/calendario' element={<Calendario/>}/>
+      <Route exact path='/masajes' element={<Masajes/>}/>
+      <Route exact path='/contacto' element={<Contacto/>}/>
+
+    </Routes>
+
+    </Router>
   );
 }
 
---
+++ src/Pages/Masajes.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Masajes = () => {
+  return (
+    <div>Masajes</div>
+  )
+}
+
+export default Masajes
